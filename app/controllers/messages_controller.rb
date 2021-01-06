a39eb605284f4ecd29aa8ebc3f1a2f9f65e898db
lauretteroy <laurieroy.dev@gmail.com>
2021-01-06 03:11:05 -0700

    Add message

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ActionController
+  before_action :require_user
+  def show
+  end
+
+  def index
+    
+  end
+
+  def create
+    
+  end
+
+  def destroy
+    
+  end
+end
